b2561de3fcbb6220ccf236a1729bf8898d232acf
KrouriB <brice.krouri@gmail.com>
2023-02-22 16:58:19 +0100

    avancement theme

--- script.js
+++ script.js
@@ -18,14 +18,58 @@ for(let i = 0 ; i < liHeader.length ;i++){
 	})
 }
 
+const themes = {
+	base: {
+		"--primary": "#1294b8",
+		"--secondary": "white",
+		"--third": "whitesmoke",
+		"--bg1": "radial-gradient(rgb(139, 195, 245), var(--primary))",
+		"--button": "#3f8efd",
+		"--btjs": "gray",
+		"--ombre": "black",
+	},
+	dark: {
+		"--primary": "#2a2a2a",
+		"--secondary": "#3f8efd",
+		"--third": "#383838",
+		"--bg1": "radial-gradient(#808080, var(--primary))",
+		"--button": "black",
+		"--btjs": "lightgray",
+		"--ombre": "white",
+	},
+};
+
 let colorswap = document.getElementById("colorswap");
 const para = document.createElement( "div" );
 const corps = document.body
 para.setAttribute("id","parabox");
-console.log(para);
-console.log(corps)
+const themeClaire = document.createElement( "div" );
+themeClaire.setAttribute("id","claire");
+themeClaire.classList.add("btnColor");
+themeClaire.innerText = "Thème Claire";
+themeClaire.dataset.theme="base";
+const themeSombre = document.createElement( "div" );
+themeSombre.setAttribute("id","sombre");
+themeSombre.classList.add("btnColor");
+themeSombre.innerText = "Thème Sombre";
+themeSombre.dataset.theme="dark";
+para.appendChild(themeClaire);
+para.appendChild(themeSombre);
+
 function colorSettings() {
 	corps.appendChild(para);
-	corps.style.overflow = "hidden"
-	corps.style.height = "100%"
+	corps.style.overflow = "hidden";
+	corps.style.height = "100%";
 }
+
+[...document.querySelectorAll('.btnColor')].forEach(el => {
+    el.addEventListener('click', () => {
+        const theme = themes[el.dataset.theme];
+        for (var variable in theme) {
+            document.documentElement.style.setProperty(variable, theme[variable]);
+			corps.removeChild(para);
+			corps.style.overflow = "scroll";
+			corps.style.height = "";
+        };
+    });
+});
