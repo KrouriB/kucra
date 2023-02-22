01950bfe377de14f168ea16747d4882fa42b2651
KrouriB <brice.krouri@gmail.com>
2023-02-22 13:57:14 +0100

    ajout d'event uncheck du menu burger

--- script.js
+++ script.js
@@ -9,3 +9,13 @@ function topFunction() {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
     }
+
+var ulHeader = document.getElementById("ulNav");
+var menuToggle = document.getElementById("menu__toggle");
+const liHeader = ulHeader.querySelectorAll("a");
+for(let i = 0 ; i < liHeader.length ;i++){
+	liHeader[i].addEventListener("click", function() {
+		menuToggle.checked = false;
+	})
+}
+
