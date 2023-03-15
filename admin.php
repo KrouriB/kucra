63a6afd9777a834d4256f182d9eb48e197c7856b
KrouriB <brice.krouri@gmail.com>
2023-03-15 11:11:34 +0100

    ajout lien pour vider le tableau email de la BDD

--- admin.php
+++ admin.php
@@ -10,28 +10,28 @@ require_once('db-functions.php');
         <?php
         $allPricing = selectAllPricing();
         $pricings = findAll($allPricing);
-        foreach ($pricings as $pricing) {?>
-            <div class="carteModif" id="carte<?=$pricing['id_pricing']?>">
-                <form  action="traitemant.php?action=update&id=<?=$pricing['id_pricing']?>" method="post" class="formCarte" id="form<?=$pricing['id_pricing']?>">
+        foreach ($pricings as $pricing) { ?>
+            <div class="carteModif" id="carte<?= $pricing['id_pricing'] ?>">
+                <form action="traitemant.php?action=update&id=<?= $pricing['id_pricing'] ?>" method="post" class="formCarte" id="form<?= $pricing['id_pricing'] ?>">
                     <div class="uneModif">
                         <label for="name">Name :</label>
-                        <input type="text" name="name" value="<?=$pricing['nom_pricing']?>" required>
+                        <input type="text" name="name" value="<?= $pricing['nom_pricing'] ?>" required>
                     </div>
                     <div class="uneModif">
                         <label for="price">Price :</label>
-                        <input type="number" name="price" value="<?=$pricing['prix_pricing']?>" required>
+                        <input type="number" name="price" value="<?= $pricing['prix_pricing'] ?>" required>
                     </div>
                     <div class="uneModif">
                         <label for="sale">Sale :</label>
-                        <input type="number" name="sale" value="<?=$pricing['pourcentage_reduction_pricing']?>" required>
+                        <input type="number" name="sale" value="<?= $pricing['pourcentage_reduction_pricing'] ?>" required>
                     </div>
                     <div class="uneModif">
                         <label for="bandwidth">Bandwidth :</label>
-                        <input type="number" name="bandwidth" value="<?=$pricing['bandwidth_pricing']?>" required>
+                        <input type="number" name="bandwidth" value="<?= $pricing['bandwidth_pricing'] ?>" required>
                     </div>
                     <div class="uneModif">
                         <label for="onlinespace">Onlinespace :</label>
-                        <input type="number" name="onlinespace" value=<?=$pricing['onlinespace_pricing']?> required>
+                        <input type="number" name="onlinespace" value=<?= $pricing['onlinespace_pricing'] ?> required>
                     </div>
                     <div class="uneModif">
                         <label>Support :</label>
@@ -40,12 +40,12 @@ require_once('db-functions.php');
                                 '<div> <input type="radio" value="1" name="support" id="sup_false" checked> <label for="support">No</label> </div>' .
                                 '<div> <input type="radio" value="0" name="support" id="sup_true"> <label for="support">Yes</label> </div>' :
                                 '<div> <input type="radio" value="1" name="support" id="sup_false"> <label for="support">No</label> </div>' .
-                                '<div> <input type="radio" value="0" name="support" id="sup_true" checked> <label for="support">Yes</label> </div>' ; ?>
+                                '<div> <input type="radio" value="0" name="support" id="sup_true" checked> <label for="support">Yes</label> </div>'; ?>
                         </div>
                     </div>
                     <div class="uneModif">
                         <label for="">Domain :</label>
-                        <input type="number" name="domain" value="<?=$pricing['domain_pricing']?>" required>
+                        <input type="number" name="domain" value="<?= $pricing['domain_pricing'] ?>" required>
                     </div>
                     <div class="uneModif">
                         <label>Hidden Fees :</label>
@@ -54,46 +54,24 @@ require_once('db-functions.php');
                                 '<div> <input type="radio" value="1" name="hidden_fees" id="h_f_false" checked> <label for="hidden_fees">No</label> </div>' .
                                 '<div> <input type="radio" value="0" name="hidden_fees" id="h_f_true"> <label for="hidden_fees">Yes</label> </div>' :
                                 '<div> <input type="radio" value="1" name="hidden_fees" id="h_f_false"> <label for="hidden_fees">No</label> </div>' .
-                                '<div> <input type="radio" value="0" name="hidden_fees" id="h_f_true" checked> <label for="hidden_fees">Yes</label> </div>' ; ?>
+                                '<div> <input type="radio" value="0" name="hidden_fees" id="h_f_true" checked> <label for="hidden_fees">Yes</label> </div>'; ?>
                         </div>
                     </div>
-                    <input type="submit" class="submitBtn" name="submit<?=$pricing['id_pricing']?>" value="Update">
+                    <input type="submit" class="submitBtn" name="submit<?= $pricing['id_pricing'] ?>" value="Update">
                 </form>
-                <span>Nombre de commande : <?=$pricing['compte_pricing']?></span>
-                <a href="traitemant.php?action=deleteCompte&id=<?=$pricing['id_pricing']?>" class="viderCompte">Vider</a>
+                <span>Nombre de commande : <?= $pricing['compte_pricing'] ?></span>
+                <a href="traitemant.php?action=deleteCompte&id=<?= $pricing['id_pricing'] ?>" class="viderCompte">Vider</a>
             </div>
         <?php
         }
         ?>
     </div>
+    <a href="traitemant.php?action=videzEmail" id="videzEmail"><i class="fa-solid fa-trash"></i></a>
     <a href="index.php" id="retourAdmin">Retour</a>
 </section>
 
 <?php
-    $content = ob_get_clean();
-    $titre="Admin Kukra";
-    require "template.php";
+$content = ob_get_clean();
+$titre = "Admin Kukra";
+require "template.php";
 ?>
-
-<!-- <div class="carteModif" id="carteG">
-
-    </div>
-    <div class="carteModif" id="carteM">
-
-    </div>
-    <div class="carteModif" id="carteD">
-
-    </div> 
-
-<div class="uneModif"><label></label><input></div>
-<div class="uneModif"><label></label><input></div>
-<div class="uneModif"><label></label><input></div>
-<div class="uneModif"><label></label><input></div>
-<div class="uneModif"><label></label><input></div>
-<div class="uneModif"><label></label><input></div>
-<div class="uneModif"><label></label><input></div>
-<div class="uneModif"><label></label><input></div>
-<input type="submit">
-
--->
-<!-- UPDATE pricing SET -->
